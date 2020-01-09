5278409d87c32ca1b3741b6be1f59b5a1c117157
weiliangliang <weiliangliang@paixueche.com>
2020-01-09 20:12:01 +0800

    flink 统计单词 success

--- src/main/java/com/example/flink/job/SocketWindowWordCount.java
+++ src/main/java/com/example/flink/job/SocketWindowWordCount.java
@@ -35,7 +35,7 @@ public class SocketWindowWordCount {
         // get the execution environment
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         // get input data by connecting to the socket
-        DataStream<String> text = env.socketTextStream(hostname, port, "\\n");
+        DataStream<String> text = env.socketTextStream(hostname, port, "\n");
         // parse the data, group it, window it, and aggregate the counts
         DataStream<WordWithCount> windowCounts = text
                 .flatMap(new FlatMapFunction<String, WordWithCount>() {
